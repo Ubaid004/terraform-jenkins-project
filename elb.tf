7312747a875a53fcf7c0b133b4de6b2b855be1a0
UBAID KHAN <75469619+Ubaid004@users.noreply.github.com>
2023-07-11 12:44:29 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -1,5 +1,5 @@
-resource "aws_elb" "bar" {
-  name               = "raham-terraform-elb"
+resource "aws_elb" "zoro-elb" {
+  name               = "zoro-elb"
   availability_zones = ["us-east-1a", "us-east-1b"]
 
   listener {
@@ -17,22 +17,22 @@ resource "aws_elb" "bar" {
     interval            = 30
   }
 
-  instances                 = ["${aws_instance.one.id}", "${aws_instance.two.id}"]
+  instances                 = ["${aws_instance.zoro.id}", "${aws_instance.nami.id}"]
   cross_zone_load_balancing = true
   idle_timeout              = 400
   tags = {
-    Name = "raham-tf-elb"
+    Name = "Zoro-elb"
   }
 }
 
  /*resource "aws_rds_cluster" "default" {
-  cluster_identifier      = "aurora-cluster-demo"
+  cluster_identifier      = "aurora-cluster-zoro"
   engine                  = "aurora-mysql"
   engine_version          = "5.7.mysql_aurora.2.11.2"
   availability_zones      = ["us-east-1a", "us-east-1b"]
-  database_name           = "mydb"
-  master_username         = "foo"
-  master_password         = "Rahamshaik#444555"
+  database_name           = "zoro-db"
+  master_username         = "zoro"
+  master_password         = "#Ambakatana004"
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
 }*/
